0de2945a2acd9eee00435d8ef2d3435ead2a9b0d
Alishetab <65300442+Alishetab@users.noreply.github.com>
2020-05-14 00:12:37 +0700

    first commit

---
+++ app/src/main/java/com/example/mysecondtry/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mysecondtry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    protected void onResume(){
+        super.onResume();
+        Toast.makeText(getApplicationContext(),"حالت از سرگیری",Toast.LENGTH_LONG).show();
+    }
+    protected void onPause(){
+        super.onPause();
+        Toast.makeText(getApplicationContext(),"حالت توقف",Toast.LENGTH_LONG).show();
+    }
+    protected void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(),"حالت تخریب",Toast.LENGTH_SHORT).show();
+    }
+}
